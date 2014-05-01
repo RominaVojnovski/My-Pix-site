d061d89db340b3f2fce2a817145c49ed49f068dc
vojno81 <romina.vojnovski@gmail.com>
2014-05-01 07:57:06 -0400

    My Pix solution commit

---
+++ dbo.Photos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Photos] (
+    [PhotoID]    INT            IDENTITY (1, 1) NOT NULL,
+    [WhenPosted] SMALLDATETIME  NOT NULL,
+    [Title]      NVARCHAR (50)  NOT NULL,
+    [Photo]      NVARCHAR (MAX) NOT NULL,
+    [Poster]     INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([PhotoID] ASC),
+    CONSTRAINT [FK_Photos_Posters] FOREIGN KEY ([Poster]) REFERENCES [dbo].[Posters] ([Id])
+);
+
---
+++ dbo.Posters.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Posters] (
+    [Id]       BIGINT            IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (50)  NOT NULL,
+    [UserName]       NVARCHAR (50)  NOT NULL,
+    [Email]          NVARCHAR (50)  NOT NULL,
+    [PasswordHash]       NVARCHAR (500)  NOT NULL,
+    [User_profile]   NVARCHAR (MAX) NULL,
+    [ADA]            BIT            DEFAULT ((0)) NOT NULL,
+    [IsConfirmed]      BIT            DEFAULT ((0)) NOT NULL,
+    [WhenRegistered] SMALLDATETIME  NOT NULL,
+    [WhenConfirmed]  SMALLDATETIME  DEFAULT (NULL) NULL,
+    [SecurityStamp] NVARCHAR(254) NULL, 
+    [Discriminator] NVARCHAR(254) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
